f7aa852b716136db2c8eac704f5bad26e52b4a75
Hossam Samir <hossamsamir@outlook.com>
2022-01-14 17:13:14 +0200

    item table

--- market.py
+++ market.py
@@ -7,6 +7,12 @@ def helle_world():
     return render_template('home.html')
 
 
-@app.route('/about/<username>')
-def about_pages(username):
-    return f'<h1>This is about page of {username}</h1>'
+@app.route('/market')
+def market_pages():
+    items=[
+        {'id':1, 'name':'Phone', 'barcode':'893212299897', 'price':500},
+        {'id':2, 'name':'laptop', 'barcode':'893212299897', 'price':900},
+        {'id':3, 'name':'Keyboard', 'barcode':'893212299897', 'price':150}
+    ]
+
+    return render_template('market.html', items=items)
