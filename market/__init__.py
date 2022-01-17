841c28b009034126fba3304b840bd3b6d93530b1
Hossam Samir <hossamsamir@outlook.com>
2022-01-17 18:31:19 +0200

    include routes

--- market/__init__.py
+++ market/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY']='bd778a7ba38c685220464a94'
 
 db = SQLAlchemy(app)
+
+from market import routes
